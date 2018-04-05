be2771538056e2475bc4becc8e5c1a254597ef2f
LisaHuang <1449700087@qq.com>
2018-04-05 15:44:33 +0800

    完成practice10中Person.java与Student.java与Teacher.java与Klass.java的创建,打开Practice10Test.java中的注释

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,52 @@
 package practice10;
 
 public class Person {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(null != obj && this.id == ((Person)obj).getId()) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,71 @@
 package practice10;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class Teacher extends Person{
+
+    private Set<Klass> classes;
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Set<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age, Set<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        StringBuilder introduceInfo = new StringBuilder();
+        introduceInfo.append(super.introduce()).append(" I am a Teacher. I teach ");
+
+        if (classes == null || classes.size() == 0) {
+            return introduceInfo.append("No Class.").toString();
+        }
+
+        List<Integer> classesNumber = new ArrayList<>();
+        introduceInfo.append("Class ");
+        for (Klass klass: classes) {
+            classesNumber.add(klass.getNumber());
+        }
+
+        Collections.sort(classesNumber);
+        for (int i = 0; i < classesNumber.size(); i++) {
+            introduceInfo.append(classesNumber.get(i));
+            if (i != classesNumber.size() - 1) {
+                introduceInfo.append(", ");
+            } else {
+                introduceInfo.append(".");
+            }
+        }
+
+        return introduceInfo.toString();
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass: classes) {
+            if (student.getKlass().equals(klass)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }

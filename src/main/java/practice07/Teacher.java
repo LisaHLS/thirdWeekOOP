d15769b6c05e52ca1574fc57fd842f3f360cb678
LisaHuang <1449700087@qq.com>
2018-04-05 13:16:41 +0800

    完成practice07中Person.java与Student.java与Teacher.java与Klass.java的创建，打开Practice07Test.java中的注释

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        if (klass.getNumber() == student.getKlass().getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }

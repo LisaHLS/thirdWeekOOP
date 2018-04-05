5babfc32eedbfc274d1f9d95461ce3bafde37fda
LisaHuang <1449700087@qq.com>
2018-04-05 14:46:43 +0800

    完成practice09中Person.java与Student.java与Teacher.java与Klass.java的创建

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,44 @@
 package practice09;
 
 public class Klass {
+
+    private int number;
+
+    private Student leader;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().getNumber() == this.number) {
+            setLeader(student);
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }

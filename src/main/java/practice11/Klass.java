59ed6f0db38a15ae14c7cf7283bde41a4fb2c7c7
LisaHuang <1449700087@qq.com>
2018-04-05 18:57:28 +0800

    完成practice11中Person.java与Student.java与Teacher.java与Klass.java的创建,打开Practice11Test.java中的注释

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,68 @@
 package practice11;
 
 public class Klass {
+
+    private int number;
+
+    private Student leader;
+
+    private Teacher teacher;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (isIn(student)) {
+            setLeader(student);
+            if (this.teacher != null) {
+                System.out.print("I am "+ this.teacher.getName() +". I know "+ student.getName() +" become Leader of "+ this.getDisplayName() +".\n");
+            }
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        if (this.teacher != null) {
+            System.out.print("I am "+ this.teacher.getName() +". I know "+ student.getName() +" has joined "+ this.getDisplayName() +".\n");
+        }
+    }
+
+    public boolean isIn(Student student) {
+        if (student.getKlass().getNumber() == this.number) {
+            return true;
+        }
+        return false;
+    }
+
 }

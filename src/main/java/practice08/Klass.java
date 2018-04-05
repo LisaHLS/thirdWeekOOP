ff7734b60a26e67d6b5b0cd0a9a5d112f7c1206a
LisaHuang <1449700087@qq.com>
2018-04-05 14:01:38 +0800

    完成practice08中Person.java与Student.java与Teacher.java与Klass.java的创建，打开Practice08Test.java中的注释

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,36 @@
 package practice08;
 
 public class Klass {
+
+    private int number;
+
+    private Student leader;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        setLeader(student);
+    }
 }
